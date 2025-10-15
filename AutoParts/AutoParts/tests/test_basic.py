0474b7afc0b73ac92de356140dafb34abd16ba8c
ValentinaAstudillo <v.astudillo@duocuc.cl>
2025-10-14 21:37:40 -0300

    Update test_basic.py

--- AutoParts/AutoParts/tests/test_basic.py
+++ AutoParts/AutoParts/tests/test_basic.py
@@ -1,6 +1,5 @@
 def test_sum():
     assert 1 + 1 == 2
 
-
-def test_fail_example():
-    assert 1 == 2  # esto fallará a propósito para probar SonarCloud
+def test_pass_example():
+    assert 2 * 2 == 4
